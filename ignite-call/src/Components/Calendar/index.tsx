c07d4d7476e0b0cc73fa8aee70895374ab137717
todpig <luccasraffael6@gmail.com>
2023-07-27 16:39:40 -0300

    gerando calendario e selecionando dias

--- ignite-call/src/Components/Calendar/index.tsx
+++ ignite-call/src/Components/Calendar/index.tsx
@@ -8,21 +8,113 @@ import {
   CalendarTitle,
 } from "./styles";
 import { getWeekDays } from "@/utils/get-week-days";
-import { truncate } from "fs/promises";
+import { useMemo, useState } from "react";
+import dayjs from "dayjs";
 
-export function Calendar() {
+interface CalendarWeek {
+  week: number;
+  days: Array<{
+    date: dayjs.Dayjs;
+    disabled: boolean;
+  }>;
+}
+
+type CalendarWeeks = CalendarWeek[];
+
+interface CalendarProps {
+  selectedDate: Date | null;
+  onDateSelected: (data: Date) => void;
+}
+
+export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set("date", 1);
+  });
+
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, "month");
+    setCurrentDate(previousMonthDate);
+  }
+
+  function handleNextMouth() {
+    const previousMonthDate = currentDate.add(1, "month");
+    setCurrentDate(previousMonthDate);
+  }
+
   const shortweekDays = getWeekDays({ short: true });
+
+  const currentMonth = currentDate.format("MMMM");
+  const currentYear = currentDate.format("YYYY");
+
+  const calendarWeek = useMemo(() => {
+    const daysInMounthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, i) => {
+      return currentDate.set("date", i + 1);
+    });
+    const firstweekDay = currentDate.get("day");
+    const previusMonthFillArray = Array.from({
+      length: firstweekDay,
+    })
+      .map((_, i) => {
+        return currentDate.subtract(i + 1, "day");
+      })
+      .reverse();
+
+    const lastDayInCurrentMonth = currentDate.set(
+      "date",
+      currentDate.daysInMonth()
+    );
+    const lastWeekDay = lastDayInCurrentMonth.get("day");
+
+    const nextMonthFillArray = Array.from({
+      length: 7 - (lastWeekDay + 1),
+    }).map((_, i) => {
+      return lastDayInCurrentMonth.add(i + 1, "day");
+    });
+
+    const calendarDays = [
+      ...previusMonthFillArray.map((date) => {
+        return { date, disabled: true };
+      }),
+      ...daysInMounthArray.map((date) => {
+        return { date, disabled: date.endOf("day").isBefore(new Date()) };
+      }),
+      ...nextMonthFillArray.map((date) => {
+        return { date, disabled: true };
+      }),
+    ];
+
+    const calendarWeeks = calendarDays.reduce<CalendarWeeks>(
+      (weeks, _, i, original) => {
+        const isNewWeek = i % 7 === 0;
+
+        if (isNewWeek) {
+          weeks.push({
+            week: i / 7 + 1,
+            days: original.slice(i, i + 7),
+          });
+        }
+
+        return weeks;
+      },
+      []
+    );
+
+    return calendarWeeks;
+  }, [currentDate]);
+
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Julho <span>2023</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previous Mounth">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMouth} title="Next Mounth">
             <CaretRight />
           </button>
         </CalendarActions>
@@ -36,36 +128,24 @@ export function Calendar() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>
-              <CalendarDay>1</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay disabled>2</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay>3</CalendarDay>
-            </td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-            <td>
-              <CalendarDay>8</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay disabled>9</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay>10</CalendarDay>
-            </td>
-          </tr>
+          {calendarWeek.map(({ week, days }) => {
+            return (
+              <tr key={week}>
+                {days.map(({ date, disabled }) => {
+                  return (
+                    <td key={date.toString()}>
+                      <CalendarDay
+                        onClick={() => onDateSelected(date.toDate())}
+                        disabled={disabled}
+                      >
+                        {date.get("date")}
+                      </CalendarDay>
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
         </tbody>
       </CalendarBody>
     </CalendarContainer>
--- ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -6,16 +6,25 @@ import {
   TimePikerItem,
   TimePikerList,
 } from "./styles";
+import { useState } from "react";
+import dayjs from "dayjs";
 
 export function CalendarStep() {
-  const isDateSelcted = true;
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const isDateSelcted = !!selectedDate;
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format("dddd") : null;
+  const describedate = selectedDate
+    ? dayjs(selectedDate).format("DD[ de ]MMMM")
+    : null;
+
   return (
     <Container isTimePickerOpen={isDateSelcted}>
-      <Calendar />
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
       {isDateSelcted && (
         <TimePiker>
           <TimePikerHeader>
-            terça-feira <span>20 de setembro</span>
+            {weekDay} <span>{describedate}</span>
           </TimePikerHeader>
           <TimePikerList>
             <TimePikerItem>08:00h</TimePikerItem>
